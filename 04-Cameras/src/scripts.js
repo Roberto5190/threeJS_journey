5e6df78be77b52638b770e2492b3af8ad7e5a27b
Roberto5190 <rfernandez5190@esdmadrid.es>
2025-02-10 20:43:28 +0100

    add orthographic camera and perspective camera(with cursor event)

--- 04-Cameras/src/scripts.js
+++ 04-Cameras/src/scripts.js
@@ -1,4 +1,23 @@
+
+
 import * as  THREE from 'three'
+
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - .5 //de esta manera el valor va de -0.5 a 0.5
+    cursor.y = e.clientY / sizes.height - .5
+})
 
 
 // Canvas
@@ -11,22 +30,28 @@ const scene = new THREE.Scene()
 
 //Objects
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 scene.add(cube)
 
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
 
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+// // .OrthographicCamera(left, right, top, bottom, near, far)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
+
+
+//.PerspectiveCamera(FOV(vertical angle, º), aspect ratio(width render/height render), near, far)
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, .1, 100) //(FOV 45-75)
+
+
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 3
-camera.position.y = .5
+camera.lookAt(cube.position)
+
 scene.add(camera)
 
 //Renderer
@@ -48,8 +73,11 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime(1)
 
     // Update Objects
-    cube.rotation.y = elapsedTime
+    // cube.rotation.y = elapsedTime
 
+    // Update Camera
+    camera.position.x = cursor.x
+    camera.position.y = cursor.y
 
     //Render
     renderer.render(scene, camera)
